2b9b033e755a0342265e43143a77392191952e03
AlejandroBrites <104328707+AlejandroBrites@users.noreply.github.com>
2022-10-13 11:51:43 -0300

    Update MyTurtle.java

--- MyTurtle.java
+++ MyTurtle.java
@@ -3,36 +3,100 @@ public class MyTurtle extends BaseTurtle {
 
 
 	public void draw() {
-
-  	//----------- Example 1:  A triangle ---------------
   	
   		this.pause(2); 	
 		this.reset();
 		
+		this.setDelay(20);
+		
+		
+		pen.setColor(45, 0, 0); 
 		pen.up(); 
-		this.back(4);
-		this.turn(270);
-		this.forward(8); 
-		pen.down(); 
-		this.forward(8); 
-		this.turn(90);
-		this.forward(8); 
-		this.turn(90);
-		this.forward(8); 
-		this.turn(90);
-		this.forward(8); 
+		this.moveTo(0,0);
 		
-
+		for (int i = 0, j = 1; i < 360; i = i + j, j++) {
 		
-
-
-	//------------ Example 2: Many squares ------------------
-
+			pen.up();
+			this.moveTo(0,0); 
+			face(i);
+			pen.down(); 
+			this.forward(20); 
 		
-
-	//------------- Example 3: Many lines ---------------
+		}
+  	
+  		this.pause(2); 	
+		this.reset();
+		
+		this.setDelay(10);
+		
+		
+		pen.setColor(45, 0, 0); 
+		pen.up(); 
+		this.moveTo(0,0);
+		
+		for (int i = 0; i < 360; i = i + 2) {
+		
+			pen.up();
+			this.moveTo(0,0); 
+			face(i);
+			pen.down(); 
+			this.forward(20); 
+		
+		}
+		
+		this.pause(2); 	
+		this.reset();
+		
+		pen.setColor(0, 0, 45); 
+		pen.up(); 
+		
+		this.turn(45);
+		
+		for (int i = -30; i < 15; i++) {
+		
+			pen.up(); 
+			this.moveTo(-10,i);
+			pen.down(); 
+			this.forward(40); 
+		
+		}
+		
+		this.turn(90);
+		
+		for (int i = -30; i < 30; i++) {
+		
+			pen.up(); 
+			this.moveTo(i,-10);
+			pen.down(); 
+			this.forward(40); 
+		
+		}
+		
+		this.turn(315);
+		
+		for (int i = -10; i < 30; i++) {
+		
+			pen.up(); 
+			this.moveTo(i,-10);
+			pen.down(); 
+			this.forward(40); 
+		
+		}
+		
+		this.turn(90);
+		
+		for (int i = -10; i < 30; i++) {
+		
+			pen.up(); 
+			this.moveTo(10,i);
+			pen.down(); 
+			this.forward(40); 
+		
+		}
+		
+		this.pause(2); 	
+		this.reset();
 		
 	}
 
-	
 }
